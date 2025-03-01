7a83d16507c336812e07c6454b787f4be8fa7bca
Alexandr Tantsiura <o.tantsiura@spd-ukraine.com>
2025-03-01 22:36:14 +0200

    Fix themes

--- screen-saver/src/src/app.tsx
+++ screen-saver/src/src/app.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
 import TimeWidget from '../components/TimeWidget/TimeWidget'
 import WeatherWidget from '../components/WeatherWidget/WeatherWidget'
 import './app.scss'
 
 function App() {
+  const [theme, setTheme] = useState(() =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return <>
       <div className='app'>
         <WeatherWidget />
         <TimeWidget />
+
+        <button
+          className='toggle-theme-button flat-button'
+          onClick={toggleTheme}>
+          Change theme to {theme === "light" ? "🌙" : "☀️"}
+        </button>
       </div>
   </>
 }
